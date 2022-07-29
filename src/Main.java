c74997731ef3c691a87eb0706e48d895b76111f2
Benhur Padilha <benhurismaelp@gmail.com>
2022-07-29 17:44:30 -0300

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import br.com.dio.desafio.dominio.Conteudo;
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -21,8 +19,41 @@ public class Main {
         mentoria1.setDescricao("Descrição Mentoria de Java");
         mentoria1.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria1);
+        System.out.println(mentoria1);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Java Developer");
+        bootcamp.setDescricao("Descrição do Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria1);
+
+        Dev devBenhur = new Dev();
+        devBenhur.setNome("Benhur");
+        devBenhur.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos Benhur: " + devBenhur.getConteudosInscritos());
+        devBenhur.progredir();
+        devBenhur.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos inscritos Benhur: " + devBenhur.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos Benhur: " + devBenhur.getConteudosConcluidos());
+        System.out.println("XP: " + devBenhur.calcularTotalXp());
+
+        System.out.println("-------------");
+
+        Dev devZezinho = new Dev();
+        devZezinho.setNome("Zezinho");
+        devZezinho.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos Zezinho: " + devZezinho.getConteudosInscritos());
+        devZezinho.progredir();
+        devZezinho.progredir();
+        devZezinho.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos inscritos Zezinho: " + devZezinho.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos Zezinho: " + devZezinho.getConteudosConcluidos());
+        System.out.println("XP: " + devZezinho.calcularTotalXp());
+
     }
 }
